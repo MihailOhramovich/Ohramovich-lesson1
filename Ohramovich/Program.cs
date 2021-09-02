88774d708cd861bc724a5db0b72a8952fa702776
Oxram <mihailohramovich@yandex.ru>
2021-09-03 01:15:11 +0300

    Готово

--- Ohramovich/Program.cs
+++ Ohramovich/Program.cs
@@ -8,8 +8,41 @@ namespace Ohramovich
 {
     class Program
     {
+        { static void Pause()
+        {
+            Console.ReadKey();
+        }
+
+        
         static void Main(string[] args)
         {
+            #region Задача 3 Охрамович
+            //а) Написать программу, которая подсчитывает расстояние между точками с координатами x1, y1 и x2,y2 по формуле r=Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2). Вывести результат, используя спецификатор формата .2f (с двумя знаками после запятой);
+            
+            Console.WriteLine("Координаты первой точки: х= \n");
+            var x1 = Console.ReadLine();
+            Console.WriteLine("Координаты первой точки: y= \n");
+            var y1 = Console.ReadLine();
+            Console.WriteLine("Координаты второй точки: х= \n");
+            var x2 = Console.ReadLine();
+            Console.WriteLine("Координаты второй точки: y= \n");
+            var y2 = Console.ReadLine();
+
+
+            var r = Math.Sqrt(Math.Pow(Convert.ToDouble(x2) - Convert.ToDouble(x1), 2) + Math.Pow(Convert.ToDouble(y2) - Convert.ToDouble(y1), 2));
+            Console.WriteLine("Расстояние между точками = " + "{0:F2}", r);
+            Pause();
+
+
+
+
+            
+         
+
+
+
+
+            #endregion
         }
     }
 }
---
+++ Task 5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    class Program
+    {
+
+        static void Pause()
+        {
+            Console.ReadKey();
+        }
+
+        #region Задача 4 Охрамович
+        // Написать программу обмена значениями двух переменных типа int без использования вспомогательных методов
+        static void Main(string[] args)
+        {
+            int num1 = 2;
+            int num2 = 1;
+            int num3 = 0;
+            Console.WriteLine(" " + num1 + " " + num2);
+
+
+            num3 = num1;
+            num1 = num2;
+            num2 = num3;
+            Console.WriteLine(" " + num1 + " " + num2);
+            Pause();
+
+            #endregion
+        }
+    }
+}
